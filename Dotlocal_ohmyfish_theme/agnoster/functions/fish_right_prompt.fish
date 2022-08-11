0800dca1d197261de9a264e57a8dbb99b57f4ec3
Ray7808 <yuwang7808@gmail.com>
2022-08-11 15:29:49 +0800

    tmux setting

--- Dotlocal_ohmyfish_theme/agnoster/functions/fish_right_prompt.fish
+++ Dotlocal_ohmyfish_theme/agnoster/functions/fish_right_prompt.fish
@@ -4,9 +4,9 @@ function fish_right_prompt
     set -l symbol_color (set_color blue)
 
     if git_is_repo
-        set root_folder (command git rev-parse --show-toplevel 2> /dev/null)
-        set parent_root_folder (dirname $root_folder)
-        set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
+        #set root_folder (command git rev-parse --show-toplevel 2> /dev/null)
+        #set parent_root_folder (dirname $root_folder)
+        #set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
     else
         set cwd (prompt_pwd)
     end
