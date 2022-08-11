57d58cee7b02796daae91797f87e0a5549230fe2
Ray7808 <yuwang7808@gmail.com>
2022-08-11 18:23:34 +0800

    Revise the style of omf theme

--- Dotlocal_ohmyfish_theme/agnoster/functions/fish_right_prompt.fish
+++ Dotlocal_ohmyfish_theme/agnoster/functions/fish_right_prompt.fish
@@ -3,13 +3,13 @@ function fish_right_prompt
     set -l cwd_color (set_color blue)
     set -l symbol_color (set_color blue)
 
-    if git_is_repo
+    #if git_is_repo
         #set root_folder (command git rev-parse --show-toplevel 2> /dev/null)
         #set parent_root_folder (dirname $root_folder)
         #set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-    else
-        set cwd (prompt_pwd)
-    end
+    #else
+    #    set cwd (prompt_pwd)
+    #end
 
     echo -n -s $cwd_color "$cwd"
     set_color --dim
@@ -24,11 +24,15 @@ function fish_right_prompt
     else if test $S -gt 1
         echo -n -s $S s
     else
-        echo -n -s $CMD_DURATION ms
+        set_color -b white
+        set_color cyan
+        #echo -n -s $CMD_DURATION ms
+        echo -n $CMD_DURATION ms
     end
     set_color normal
-    
-  set_color white
+  
+  set_color -b white  
+  set_color black
   printf '[%s]' (date +%H:%M:%S)
   set_color normal
 end
